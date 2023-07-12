52dc67c10f05229f180f25dc79e2314ac555b418
Pavan Mekala <pavanmekala64@gmail.com>
2023-07-12 14:58:26 +0530

    API's updated

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -10,19 +10,19 @@ export class EmployeeService {
   constructor(private  _http:HttpClient) { }
 
   addEmployee(data: any) : Observable<any>{
-    return this._http.post('http://localhost:3000/employees',data)
+    return this._http.post('https://my-json-server-rtt0.onrender.com/employees',data)
   }
 
   updateEmployee(id:number , data: any) : Observable<any>{
-    return this._http.put(`http://localhost:3000/employees/${id}`,data)
+    return this._http.put(`https://my-json-server-rtt0.onrender.com/employees/${id}`,data)
   }
 
   getEmployeeList(): Observable<any> {
-    return this._http.get('http://localhost:3000/employees');
+    return this._http.get('https://my-json-server-rtt0.onrender.com/employees');
   }
 
   deleteEmployee(id: number): Observable<any> {
-    return this._http.delete(`http://localhost:3000/employees/${id}`)
+    return this._http.delete(`https://my-json-server-rtt0.onrender.com/employees/${id}`)
   }
 
 }
